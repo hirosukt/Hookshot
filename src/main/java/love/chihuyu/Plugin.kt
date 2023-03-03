07968d2024b8138f0813454b34e9ba4417f1105a
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-03-03 13:41:56 +0900

    base

--- src/main/java/love/chihuyu/Plugin.kt
+++ src/main/java/love/chihuyu/Plugin.kt
@@ -1,6 +1,13 @@
 package love.chihuyu
 
+import io.papermc.paper.event.entity.EntityLoadCrossbowEvent
+import org.bukkit.FluidCollisionMode
+import org.bukkit.Location
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityShootBowEvent
+import org.bukkit.metadata.FixedMetadataValue
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin : JavaPlugin(), Listener {
@@ -15,5 +22,35 @@ class Plugin : JavaPlugin(), Listener {
 
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
+    }
+
+    @EventHandler
+    fun onLoad(e: EntityLoadCrossbowEvent) {
+        val player = e.entity as? Player ?: return
+        val block = player.rayTraceBlocks(40.0, FluidCollisionMode.NEVER)?.hitBlock ?: return
+//        if (e.crossbow?.type != Material.ARROW) return
+        player.sendMessage("Hooked!")
+        player.setMetadata("hookshot_isHooked", FixedMetadataValue(this, true))
+        player.setMetadata("hookshot_hookedPoint_x", FixedMetadataValue(this, block.location.x))
+        player.setMetadata("hookshot_hookedPoint_y", FixedMetadataValue(this, block.location.y))
+        player.setMetadata("hookshot_hookedPoint_z", FixedMetadataValue(this, block.location.z))
+    }
+
+    @EventHandler
+    fun onLaunch(e: EntityShootBowEvent) {
+        val player = e.entity as? Player ?: return
+        val proj = e.projectile
+
+        if (player.getMetadata("hookshot_isHooked")[0].asBoolean()) {
+            proj.remove()
+            player.setMetadata("hookshot_isHooked", FixedMetadataValue(this, false))
+            player.teleport(Location(
+                player.world,
+                player.getMetadata("hookshot_hookedPoint_x")[0].asDouble(),
+                player.getMetadata("hookshot_hookedPoint_y")[0].asDouble(),
+                player.getMetadata("hookshot_hookedPoint_z")[0].asDouble()
+            ))
+            player.sendMessage("Jumped!")
+        }
     }
 }
